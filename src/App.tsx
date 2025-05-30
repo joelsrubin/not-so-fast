18340aed2b64bc7025ae74665bd217b0abbee36a
Joel Rubin <68345201+joelsrubin@users.noreply.github.com>
2025-05-30 11:51:04 -0400

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="min-h-screen flex items-center justify-center ">
-      <div className="max-w-2xl mx-auto p-4">
+      <div className="max-w-2xl mx-auto p-8">
         <img
           src="/logo.jpeg"
           alt="logo"
