d5d7cfa0ec9caa09c44331c7d45f6cc422eb7152
Joel Rubin <68345201+joelsrubin@users.noreply.github.com>
2025-05-30 11:15:51 -0400

    remove biome

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-
-
-
 function App() {
   return (
     <div className="min-h-screen flex items-center justify-center ">
       <div className="max-w-2xl mx-auto p-4">
-        <img 
-          src="/logo.jpeg" 
-          alt="logo" 
+        <img
+          src="/logo.jpeg"
+          alt="logo"
           className="max-w-full h-auto"
         />
       </div>
